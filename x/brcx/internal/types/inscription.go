7c946dff5a89d7376ce641c3869853ca293c9da3
BananaLF <864685021@qq.com>
2023-11-15 17:29:11 +0800

    init inscription

--- x/brcx/internal/types/inscription.go
+++ x/brcx/internal/types/inscription.go
@@ -7,13 +7,13 @@ import (
 )
 
 type InscriptionContext struct {
-	InscriptionId     string   `json:"inscription_id" yaml:"inscription_id"`         //铭文id
-	InscriptionNumber uint64   `json:"inscription_number" yaml:"inscription_number"` //铭文编号
+	InscriptionId     string   `json:"inscription_id" yaml:"inscription_id"`         // 铭文id
+	InscriptionNumber int64    `json:"inscription_number" yaml:"inscription_number"` // 铭文编号
 	IsTransfer        bool     `json:"is_transfer" yaml:"is_transfer"`               // 是否为转移铭文
 	Txid              Hash     `json:"txid" yaml:"txid"`                             // 铭文的所在交易的txid
 	Sender            string   `json:"sender" yaml:"sender"`                         // 铭文的发送者
-	Receiver          string   `json:"receiver" yaml:"receiver"`                     //铭文接收者
-	CommitInput       []TxOut  `json:"commit_input" yaml:"commit_input"`             //提交交易的输入
+	Receiver          string   `json:"receiver" yaml:"receiver"`                     // 铭文接收者
+	CommitInput       []TxIn   `json:"commit_input" yaml:"commit_input"`             //提交交易的输入
 	RevealOutput      []TxOut  `json:"reveal_output" yaml:"reveal_output"`           //揭示交易的输出
 	OldSatPoint       SatPoint `json:"old_sat_point" yaml:"old_sat_point"`           // 转移前，铭文所在的聪
 	NewSatPoint       SatPoint `json:"new_sat_point" yaml:"new_sat_point"`           // 转移后，铭文所在的聪
@@ -23,9 +23,10 @@ type InscriptionContext struct {
 	BlockHeight uint64 `json:"block_height" yaml:"block_height"` // btc block height
 }
 
+// Sat
 type SatPoint struct {
 	Outpoint Outpoint `json:"outpoint" yaml:"outpoint"` // btc的utxo
-	Offset   uint64   `json:"offset" yaml:"offset"`     // 聪数
+	Offset   uint64   `json:"offset" yaml:"offset"`     // 聪数在utxo里的偏移量
 }
 
 // UTXO
@@ -34,7 +35,7 @@ type Outpoint struct {
 	Vout uint32 `json:"vout" yaml:"vout"` // 第几个输出
 }
 
-// 复合类型
+// 交易输出
 type TxOut struct {
 	Txid     Hash   `json:"txid" yaml:"txid"`
 	Vout     uint32 `json:"vout" yaml:"vout"`
