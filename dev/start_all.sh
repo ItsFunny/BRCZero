3bbd7fd51fb76613890696a0c638f7bcd60bb7ae
ylsGit <bzwdyls@163.com>
2023-11-20 17:07:43 +0800

    Update start_all.sh

--- dev/start_all.sh
+++ dev/start_all.sh
@@ -8,12 +8,12 @@ set -a
 set -m
 
 echo "************* Start brczero node... *************"
-./testnet.sh -s -i -n 5 -r 1
-## val0 26656  26657  8545
-## val1 26756  26757  8645
-## val2 26856  26857  8745
-## val3 26956  26957  8845
-## rpc  27056  27057  8945
+./testnet.sh -s -i -n 1 ## -r 1
+# val0 26656  26657  8545
+# val1 26756  26757  8645
+# val2 26856  26857  8745
+# val3 26956  26957  8845
+# rpc  27056  27057  8945
 
 echo "************* Start btc node... *************"
 rm -rf ./bitcoin-data/regtest
@@ -40,51 +40,51 @@ nohup ./target/debug/ord \
   --first-brczero-height=120 \
   server --http-port=80 >/dev/null 2>&1 &
 
-rm -rf ./_cache2
-nohup ./target/debug/ord \
-  --log-level=DEBUG \
-  --data-dir=./_cache2 \
-  --rpc-url=http://localhost:18443 \
-  --regtest \
-  --bitcoin-rpc-user bitcoinrpc \
-  --bitcoin-rpc-pass bitcoinrpc \
-  --brczero-rpc-url=http://127.0.0.1:26757 \
-  --first-brczero-height=120 \
-  server --http-port=81 >/dev/null 2>&1 &
-
-rm -rf ./_cache3
-nohup ./target/debug/ord \
- --log-level=DEBUG \
- --data-dir=./_cache3 \
- --rpc-url=http://localhost:18443 \
- --regtest \
- --bitcoin-rpc-user bitcoinrpc \
- --bitcoin-rpc-pass bitcoinrpc \
- --brczero-rpc-url=http://127.0.0.1:26857 \
- --first-brczero-height=120 \
- server --http-port=82 >/dev/null 2>&1 &
-
-rm -rf ./_cache4
-nohup ./target/debug/ord \
---log-level=DEBUG \
---data-dir=./_cache4 \
---rpc-url=http://localhost:18443 \
---regtest \
---bitcoin-rpc-user bitcoinrpc \
---bitcoin-rpc-pass bitcoinrpc \
---brczero-rpc-url=http://127.0.0.1:26957 \
---first-brczero-height=120 \
-server --http-port=83 >/dev/null 2>&1 &
-
-rm -rf ./_cache5
-nohup ./target/debug/ord \
- --log-level=DEBUG \
- --data-dir=./_cache5 \
- --rpc-url=http://localhost:18443 \
- --regtest \
- --bitcoin-rpc-user bitcoinrpc \
- --bitcoin-rpc-pass bitcoinrpc \
- --brczero-rpc-url=http://127.0.0.1:27057 \
- --first-brczero-height=120 \
- server --http-port=84 >/dev/null 2>&1 &
+#rm -rf ./_cache2
+#nohup ./target/debug/ord \
+#  --log-level=DEBUG \
+#  --data-dir=./_cache2 \
+#  --rpc-url=http://localhost:18443 \
+#  --regtest \
+#  --bitcoin-rpc-user bitcoinrpc \
+#  --bitcoin-rpc-pass bitcoinrpc \
+#  --brczero-rpc-url=http://127.0.0.1:26757 \
+#  --first-brczero-height=120 \
+#  server --http-port=81 >/dev/null 2>&1 &
+#
+#rm -rf ./_cache3
+#nohup ./target/debug/ord \
+# --log-level=DEBUG \
+# --data-dir=./_cache3 \
+# --rpc-url=http://localhost:18443 \
+# --regtest \
+# --bitcoin-rpc-user bitcoinrpc \
+# --bitcoin-rpc-pass bitcoinrpc \
+# --brczero-rpc-url=http://127.0.0.1:26857 \
+# --first-brczero-height=120 \
+# server --http-port=82 >/dev/null 2>&1 &
+#
+#rm -rf ./_cache4
+#nohup ./target/debug/ord \
+#--log-level=DEBUG \
+#--data-dir=./_cache4 \
+#--rpc-url=http://localhost:18443 \
+#--regtest \
+#--bitcoin-rpc-user bitcoinrpc \
+#--bitcoin-rpc-pass bitcoinrpc \
+#--brczero-rpc-url=http://127.0.0.1:26957 \
+#--first-brczero-height=120 \
+#server --http-port=83 >/dev/null 2>&1 &
+#
+#rm -rf ./_cache5
+#nohup ./target/debug/ord \
+# --log-level=DEBUG \
+# --data-dir=./_cache5 \
+# --rpc-url=http://localhost:18443 \
+# --regtest \
+# --bitcoin-rpc-user bitcoinrpc \
+# --bitcoin-rpc-pass bitcoinrpc \
+# --brczero-rpc-url=http://127.0.0.1:27057 \
+# --first-brczero-height=120 \
+# server --http-port=84 >/dev/null 2>&1 &
 
