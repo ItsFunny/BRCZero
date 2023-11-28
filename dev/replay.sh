98595c41fe768df1085b981de9c8f94f8b0bb81a
BananaLF <864685021@qq.com>
2023-11-28 14:25:54 +0800

    add auto replay scripts

--- dev/replay.sh
+++ dev/replay.sh
@@ -1,5 +1,23 @@
-request=$(curl --location "http://127.0.0.1:80/api/v1/brc0/rpc_request/$1" | jq -r '.data')
-echo "{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"broadcast_brczero_txs_async\",\"params\":$request}"
-curl --location 'http://localhost:26657' \
---header 'Content-Type: application/json' \
---data "{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"broadcast_brczero_txs_async\",\"params\":$request}"
+
+start_height=$1
+end_height=$2
+
+
+run() {
+  request=$(curl -s --location "http://127.0.0.1:80/api/v1/brc0/rpc_request/$1" | jq -r '.data')
+  btchash=$(echo $request | jq -r '.block_hash')
+  if [ "$btchash" == "" ]; then
+    echo "height: $1 is not exist"
+    exit 1
+  else
+    echo "request$1: $btchash"
+    curl --location 'http://localhost:26657' \
+    --header 'Content-Type: application/json' \
+    --data "{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"broadcast_brczero_txs_async\",\"params\":$request}"
+  fi
+}
+
+for i in `seq $start_height $end_height`
+do
+   run $i
+done
