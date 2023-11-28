367ade786591f9d03e82ad8658f796e07cc95ab8
BananaLF <864685021@qq.com>
2023-11-29 01:21:54 +0800

    support repair-state change contract address. add query protocol address.

--- dev/query_contract_address.sh
+++ dev/query_contract_address.sh
@@ -1,4 +1,3 @@
 tx=$(curl -s http://localhost:8545/v1/block_tx_hashes/$1 | jq -r '.[0]')
-echo $tx
 brczerocli query tx $tx | jq -r '.'
 
--- x/brcx/internal/keeper/querier.go
+++ x/brcx/internal/keeper/querier.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"github.com/brc20-collab/brczero/libs/cosmos-sdk/codec"
 	sdk "github.com/brc20-collab/brczero/libs/cosmos-sdk/types"
 	sdkerrors "github.com/brc20-collab/brczero/libs/cosmos-sdk/types/errors"
 	abci "github.com/brc20-collab/brczero/libs/tendermint/abci/types"
@@ -11,9 +12,21 @@ import (
 func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
 		switch path[0] {
-
+		case types.QueryProtocol:
+			return queryProtocol(ctx, req, k)
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown %s query endpoint: %s", types.ModuleName, path[0])
 		}
 	}
+}
+
+func queryProtocol(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	name := string(req.Data)
+	addresses := k.GetContractAddressByName(ctx, name)
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, addresses)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
 }
