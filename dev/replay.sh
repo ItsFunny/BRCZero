b0e8541662c49ba4de49cb2d414946344e07ad0b
root <root@ip-172-31-36-208.ap-northeast-1.compute.internal>
2023-11-28 08:20:33 +0000

    add some scripts

--- dev/replay.sh
+++ dev/replay.sh
@@ -4,16 +4,18 @@ end_height=$2
 
 
 run() {
-  request=$(curl -s --location "http://127.0.0.1:80/api/v1/brc0/rpc_request/$1" | jq -r '.data')
+  request=$(curl -s --location "http://127.0.0.1:81/api/v1/brc0/rpc_request/$1" | jq -r '.data')
   btchash=$(echo $request | jq -r '.block_hash')
   if [ "$btchash" == "" ]; then
     echo "height: $1 is not exist"
     exit 1
   else
     echo "request$1: $btchash"
-    curl --location 'http://localhost:26657' \
+    res=$(echo "{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"broadcast_brczero_txs_async\",\"params\":$request}" \
+    | curl -s --location 'http://localhost:26657' \
     --header 'Content-Type: application/json' \
-    --data "{\"jsonrpc\":\"2.0\",\"id\":3,\"method\":\"broadcast_brczero_txs_async\",\"params\":$request}"
+    --data @-)
+    echo $res | jq -r '.jsonrpc'
   fi
 }
 
